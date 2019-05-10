dd5a1087b8c35b719179ea49b05bb1a82acfb506
Athiwat Hirunworawongkun <athivvat@gmail.com>
2019-05-10 14:26:21 +0700

    Add prefixPath

--- www/site-config.js
+++ www/site-config.js
@@ -5,7 +5,7 @@ module.exports = {
     siteTitleAlt: 'Gatsby Starter Template for Creating Docs', // Alternative site title for SEO.
     siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
     siteUrl: 'https://humaan-ai.github.io/sidewalk/', // Domain of your website without pathPrefix.
-    pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+    pathPrefix: 'https://humaan-ai.github.io/sidewalk/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
     siteDescription: 'A GatsbyJS starter for creating Documentation or Tutorials.', // Website description used for RSS feeds/meta description tag.
     siteRss: '/rss.xml', // Path to the RSS file.
     googleAnalyticsID: 'UA-XXXXX ', // GA tracking ID.
