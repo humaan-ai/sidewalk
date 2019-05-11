c0da1212869fd27c359f1705f7fd105e70596782
Athiwat Hirunworawongkun <athivvat@gmail.com>
2019-05-11 18:13:10 +0700

    Update site-config

--- www/site-config.js
+++ www/site-config.js
@@ -4,8 +4,8 @@ module.exports = {
     siteTitle: 'Gatsby Docs Starter', // Site title.
     siteTitleAlt: 'Gatsby Starter Template for Creating Docs', // Alternative site title for SEO.
     siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-    siteUrl: 'https://humaan-ai.github.io/sidewalk/', // Domain of your website without pathPrefix.
-    pathPrefix: 'https://humaan-ai.github.io/sidewalk/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+    siteUrl: 'https://humaan-ai.github.io', // Domain of your website without pathPrefix.
+    pathPrefix: 'sidewalk', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
     siteDescription: 'A GatsbyJS starter for creating Documentation or Tutorials.', // Website description used for RSS feeds/meta description tag.
     siteRss: '/rss.xml', // Path to the RSS file.
     googleAnalyticsID: 'UA-XXXXX ', // GA tracking ID.
